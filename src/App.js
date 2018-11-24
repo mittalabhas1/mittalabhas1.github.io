6628a83136d247ef4b42edf494e6dda9f06d7b0a
Abhas Mittal <mittalabhas1@gmail.com>
2018-11-24 13:29:21 +0530

    add content

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import abhas from './abhas.png';
 import './App.css';
 
+const socialIcons = [
+  { classname: 'linkedin-square', url: 'https://www.linkedin.com/in/mittalabhas1/' },
+  { classname: 'github-square', url: 'https://github.com/mittalabhas1/' },
+  { classname: 'twitter-square', url: 'https://twitter.com/abhas_4/' },
+  { classname: 'envelope-square', url: 'mailto:mittalabhas1@gmail.com', target: '_self' },
+];
+
 class App extends Component {
+  renderSocialIcon = (icon, i) => {
+    return (
+      <a
+        className="App-link"
+        href={icon.url}
+        target={icon.target || "_blank"}
+        rel="noopener noreferrer"
+        key={`social-icon-${i}`}
+      >
+        <i className={`fa fa-${icon.classname}`} />
+      </a>
+    )
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="name-container">
+            <span className="first-name">Abhas</span>
+            <span className="last-name">Mittal</span>
+          </div>
+          <img src={abhas} className="App-logo" alt="logo" />
+          <div className="social-container">
+            {socialIcons.map(this.renderSocialIcon)}
+          </div>
         </header>
       </div>
     );
